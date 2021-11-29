5e9c4d87176c7951aa1f5c8e4d3d9ef818becdbc
salt-dev <loukasmalias@hotmail.com>
2021-11-29 12:29:53 +0100

    burger menu animation added

--- client/src/components/Burger/Burger.js
+++ client/src/components/Burger/Burger.js
@@ -2,9 +2,20 @@ import React from 'react';
 import './Burger.css';
 
 const Burger = () => {
+  const handleClick = e => {
+    e.target.closest('div').classList.toggle('active');
+    e.target.closest('div').classList.toggle('not-active');
+  };
+
   return (
     <div className="page-header__burger">
-      H
+      <div className="box">
+        <div className="btn not-active" onClick={handleClick}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
     </div>
   );
 };
